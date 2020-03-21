4ff64b598b24172cf30b8f61120ca9d9fda9b1cd
pilagod <pilagooood@gmail.com>
2020-03-21 14:14:21 +0800

    [p1] add test for getTripsByUser to throw UserNotLoggedInError when user is not logged in

--- src/trip/TripService.test.ts
+++ src/trip/TripService.test.ts
@@ -1,5 +1,32 @@
+import { Nullable } from '@type';
+import { UserNotLoggedInError } from '@error';
+import TripService from '@trip/TripService';
+import User from '@user/User';
+
 describe('TripService', () => {
-  it('should pass this canary test', () => {
-    expect(true).toBe(true);
+
+  describe('getTripsByUser', () => {
+    it('should throw UserNotLoggedInError when use is not logged in', () => {
+      const service = new TripServiceUnderTest();
+      service.givenLoggedUser(null);
+
+      const getTripsByUser = () => service.getTripsByUser(new User());
+
+      expect(getTripsByUser).toThrowError(UserNotLoggedInError);
+    });
   });
 });
+
+class TripServiceUnderTest extends TripService {
+  private user: Nullable<User> = null;
+
+  // helper for test to change logged user
+  public givenLoggedUser(user: Nullable<User>) {
+    this.user = user;
+  }
+
+  // expect there is a method called getLoggedUser on TripService that can be overridden
+  protected getLoggedUser(): Nullable<User> {
+    return this.user;
+  }
+}
